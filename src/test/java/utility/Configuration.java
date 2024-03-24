ab3b6be03b18c1f0d4ca332c66e8527343b2d20c
Azertb <azertbini8@gmail.com>
2024-03-24 23:14:15 +0000

    Veeam assessment_Add_configuration_File

---
+++ src/test/java/utility/Configuration.java
@@ -0,0 +1,9 @@
+package utility;
+
+public class Configuration {
+    public static final String BaseUrl="https://petstore.swagger.io/";
+    public static final String GetDeletePostUrl="v2/store/order/";
+    public static final String GetInventoryUrl="v2/store/inventory";
+
+
+}
